d6057f98599eedfacd56876083b3046b33ae0a8d
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-11-11 21:07:58 -0500

    feat: remove backend

--- apollo/apolloClient.js
+++ apollo/apolloClient.js
@@ -1,167 +1,228 @@
-import { ApolloClient, ApolloLink, HttpLink, InMemoryCache, split } from '@apollo/client'
+import {
+    ApolloClient,
+    ApolloLink,
+    split
+} from '@apollo/client'
+import { onError } from '@apollo/client/link/error'
 import { WebSocketLink } from '@apollo/client/link/ws'
-import { concatPagination, getMainDefinition } from '@apollo/client/utilities'
+import { getMainDefinition } from '@apollo/client/utilities'
+import FingerprintJS from '@fingerprintjs/fingerprintjs'
 import { createUploadLink } from 'apollo-upload-client'
 import merge from 'deepmerge'
 import isEqual from 'lodash/isEqual'
+import { Cookies } from 'npm-pkg-hook'
 import { useMemo } from 'react'
-import { URL_ADMIN, URL_ADMIN_SERVER, URL_BASE } from './urls'
+import { cache, isLoggedVar } from './cache'
+import { typeDefs } from './schema'
+import { URL_ADMIN, URL_BASE_ADMIN_MASTER } from './urls'
 
 export const APOLLO_STATE_PROP_NAME = '__APOLLO_STATE__'
 
 let apolloClient
+
 export const getDeviceId = async () => {
-    return 32432
+  const fp = await FingerprintJS.load()
+  const result = await fp.get()
+  const { visitorId } = result || {}
+  return visitorId
 }
-const authLink = async (_) => {
-    if (typeof window !== "undefined") {
-        const token = window?.localStorage.getItem('session')
 
-        const authorization = token ? `Bearer ${token}` : null
-        return token
-            ? {
-                headers: {
-                    // ...headers,
-                    authorization,
-                },
-            }
-            : {
-                headers: {
-                    // ...headers,
-                },
-            }
+const removeDoubleQuotes = (text = '') => {
+  return text?.replace(/['"]+/g, '')
+}
 
+const authLink = async () => {
+  try {
+    if (typeof window !== 'undefined') {
+      const token = Cookies.get('session')
+      const restaurant = removeDoubleQuotes(Cookies.get('restaurant'))
+      return {
+        authorization: token ? `Bearer ${token}` : 'Bearer', // Corregido: Si no hay token, establece 'Bearer'
+        restaurant: restaurant ?? '', // Corregido: Usa el valor de restaurant o cadena vacía si es nulo
+        deviceid: ''
+      }
     }
+
+    return {
+      authorization: 'Bearer',
+      restaurant: '',
+      deviceid: ''
+    }
+  } catch (error) {
+    return {
+      authorization: 'Bearer',
+      restaurant: '',
+      deviceid: ''
+    }
+
+  }
 }
+
 
 // Create Second Link
-const wsLink = process.browser ? new WebSocketLink({
-    uri: process.env.NODE_ENV === 'development' ? 'ws://localhost:4000/graphql' : 'ws://server-image-food.herokuapp.com/graphql',
-    options: {
-        reconnect: true,
-        lazy: true,
-        connectionParams: () => {
-            return { headers: { Authorization: 'Bearer TOKEN' } }
-        }
+// https:graphql
+function getWebSocketHeaders() {
+  try {
+    const token = Cookies.get('session') // Obtén el token de la cookie 'session'
+    const restaurant = removeDoubleQuotes(Cookies.get('restaurant')) // Obtén el restaurante de la cookie 'restaurant'
+
+    if (!token || !restaurant) {
+      throw new Error('Authentication tokens are missing')
     }
-}) : null;
+
+    return {
+      authorization: `Bearer ${token}`,
+      restaurant: restaurant
+    }
+  } catch (error) {
+    return {
+      authorization: `Bearer`,
+      restaurant: ''
+    }
+  }
+}
+
+function getWebSocketConnectionParams() {
+  return {
+    credentials: 'include',
+    headers: getWebSocketHeaders()
+  }
+}
+
+const wsLink = typeof window !== 'undefined' ? new WebSocketLink({
+  uri: `${process.env.URL_ADMIN_SERVER_SOCKET}/graphql`,
+  options: {
+    reconnect: true,
+    lazy: true,
+    inactivityTimeout: 30000,
+    timeout: 10000,
+    connectionCallback: handleWebSocketConnectionCallback,
+    wsOptionArguments: {
+      headers: getWebSocketHeaders()
+    },
+    connectionParams: getWebSocketConnectionParams()
+  }
+}) : null
 
 
+function handleWebSocketConnectionCallback(res) {
+  // eslint-disable-next-line no-console
+  console.log('WebSocket connected:', res)
+}
 
-const getLink = async (operation) => {
-    // await splitLink({ query: operation.query })
+function createApolloClient() {
+  const errorLink = onError(({ graphQLErrors, networkError }) => {
+    if (graphQLErrors)
+      // eslint-disable-next-line no-console
+      graphQLErrors.map(({ message, location, path }) => { return console.log(`[GraphQL error]: Message: ${message}, Location: ${location}, Path: ${path}`) }
+      )
+
+    // eslint-disable-next-line no-console
+    if (networkError) console.log(`[Network error]: ${networkError}`)
+  })
+  const ssrMode = typeof window === 'undefined' // Disables forceFetch on the server (so queries are only run once)
+  const getLink = async (operation) => {
     const headers = await authLink()
     const service = operation.getContext().clientName
-    let uri = `${process.env.URL_BASE}/api/graphql`
-    if (service === 'subscriptions') uri = 'http://localhost:4000/graphql'
-    if (service === 'main') uri = `${process.env.URL_BASE}/api/graphql`
-    if (service === 'admin') uri = `${URL_ADMIN}graphql`
-    if (service === 'admin-server') uri = `${URL_ADMIN_SERVER}graphql`
-    if (service === 'admin-store') uri = `${process.env.URL_BASE}/api/graphql`
-    const link = new HttpLink({
-        uri,
-        credentials: 'same-origin',
-        authorization: '',
+    let uri = `${process.env.URL_BACK_SERVER}/graphql`
+    if (service === 'main') uri = `${process.env.URL_BACK_SERVER}/graphql`
+    if (service === 'admin-store') uri = `${URL_ADMIN}graphql`
+    if (service === 'admin') uri = `${URL_BASE_ADMIN_MASTER}graphql`
+    if (service === 'admin-server') uri = `${process.env.URL_ADMIN_SERVER_SOCKET_HTTPS}`
+    const token = Cookies.get('session')
+    const context = operation.getContext()
+    const { headers: ctx } = context || {}
+    const { restaurant } = ctx || {}
+    operation.setContext({
+      headers: {
+        ...headers,
+        authorization: `Bearer ${token}`,
+        client: 'front-admin'
+      }
+    })
+
+
+    if (!restaurant) {
+      isLoggedVar({ state: false, expired: true, message: 'Inicie session', code: 403 })
+    }
+    const link = createUploadLink({
+      uri,
+      credentials: 'same-origin',
+      authorization: service === 'admin-server' || service === 'subscriptions' ? `Bearer ${token}` : `${restaurant}`,
+
+      headers: {
         ...headers
+      }
     })
     return link.request(operation)
-}
-const httpLink = createUploadLink({
-    uri: `${URL_BASE}graphql`,
-    authorization: 'pija',
-    credentials: 'same-origin'
-})
-
-const Link = typeof window !== "undefined" ? split(
-    (operation) => {
-        const url = `${URL_BASE}graphql`
-        const service = operation.getContext().clientName
-        console.log(service)
-        const definition = getMainDefinition(operation.query);
-        return (definition.kind === 'OperationDefinition' && definition.operation === 'subscription');
-    },
-    wsLink,
-    httpLink,
-) : httpLink;
-
-const defaultOptions = {
-    watchQuery: {
-        fetchPolicy: 'cache-first',
-        returnPartialData: true,
-        notifyOnNetworkStatusChange: true,
-        errorPolicy: 'all',
+  }
+  const defaultOptions = {
+    query: {
+      errorPolicy: 'all',
+      fetchPolicy: 'cache-first'
     },
     mutate: {
-        errorPolicy: 'all'
+      errorPolicy: 'all'
     }
+  }
+  const link = ssrMode ? ApolloLink.split(() => { return true }, operation => { return getLink(operation) }
+  ) : !ssrMode
+    ? split((operation) => {
+      const definition = getMainDefinition(operation.query)
+      return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
+    },
+    wsLink,
+    ApolloLink.split(() => { return true }, operation => { return getLink(operation) },
+      errorLink
+    )
+    )
+    : ApolloLink.split(() => { return true }, operation => { return getLink(operation) },
+      errorLink
+    )
+  return new ApolloClient({
+    connectToDevTools: true,
+    ssrMode,
+    link: link,
+    defaultOptions,
+    typeDefs,
+    cache
+  })
 }
-function createApolloClient() {
-    const ssrMode = typeof window === 'undefined'
-    const link = ssrMode ? ApolloLink.split(() => true, operation => getLink(operation)) : typeof window !== "undefined"
-        ? split((operation) => {
-            const definition = getMainDefinition(operation.query)
-            return definition.kind === 'OperationDefinition' && definition.operation === 'subscription'
-        },
-            wsLink,
-            ApolloLink.split(() => true, operation => getLink(operation))
-
-        )
-        : ApolloLink.split(() => true, operation => getLink(operation))
-    return new ApolloClient({
-        connectToDevTools: true,
-        ssrMode: typeof window === 'undefined',
-        link,
-        cache: new InMemoryCache({
-            typePolicies: {
-                Query: {
-                    fields: {
-                        allPosts: concatPagination()
-                    }
-                }
-            }
-        })
+export function initializeApollo(initialState = null) {
+  const _apolloClient = apolloClient ?? createApolloClient()
+  // If your page has Next.js data fetching methods that use Apollo Client, the initial state
+  // gets hydrated here
+  if (initialState) {
+    // Get existing cache, loaded during client side data fetching
+    const existingCache = _apolloClient.extract()
+    // Merge the existing cache into data passed from getStaticProps/getServerSideProps
+    const data = merge(initialState, existingCache, {
+      // combine arrays using object equality (like in sets)
+      arrayMerge: (destinationArray, sourceArray) => {
+        return [
+          ...sourceArray,
+          ...destinationArray.filter(d => { return sourceArray.every(s => { return !isEqual(d, s) }) }
+          )
+        ]
+      }
     })
+    // Restore the cache with the merged data
+    _apolloClient.cache.restore(data)
+  }
+  // For SSG and SSR always create a new Apollo Client
+  if (typeof window === 'undefined') return _apolloClient
+  // Create the Apollo Client once in the client
+  if (!apolloClient) apolloClient = _apolloClient
+  return _apolloClient
 }
-
-export function initializeApollo(initialState = null, _ctx) {
-    const _apolloClient = apolloClient ?? createApolloClient()
-    // If your page has Next.js data fetching methods that use Apollo Client, the initial state
-    // gets hydrated here
-    if (initialState) {
-        // Get existing cache, loaded during client side data fetching
-        const existingCache = _apolloClient.extract()
-
-        // Merge the existing cache into data passed from getStaticProps/getServerSideProps
-        const data = merge(initialState, existingCache, {
-            // combine arrays using object equality (like in sets)
-            arrayMerge: (destinationArray, sourceArray) => [
-                ...sourceArray,
-                ...destinationArray.filter(d => sourceArray.every(s => !isEqual(d, s))
-                )
-            ]
-        })
-
-        // Restore the cache with the merged data
-        _apolloClient.cache.restore(data)
-    }
-    // For SSG and SSR always create a new Apollo Client
-    if (typeof window === 'undefined') return _apolloClient
-    // Create the Apollo Client once in the client
-    if (!apolloClient) apolloClient = _apolloClient
-
-    return _apolloClient
-}
-
 export function addApolloState(client, pageProps) {
-    if (pageProps?.props) {
-        pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
-    }
-
-    return pageProps
+  if (pageProps?.props) {
+    pageProps.props[APOLLO_STATE_PROP_NAME] = client.cache.extract()
+  }
+  return pageProps
 }
-
 export function useApollo(pageProps) {
-    const state = pageProps[APOLLO_STATE_PROP_NAME]
-    const store = useMemo(() => initializeApollo(state, pageProps), [pageProps, state])
-    return store
+  const state = pageProps[APOLLO_STATE_PROP_NAME]
+  const store = useMemo(() => { return initializeApollo(state) }, [state])
+  return store
 }
