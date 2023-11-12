ccbdacee131f822b29dec46b6c44d633d4840f63
Jesus Juvinao <juvinaojesusd@gmail.com>
2023-11-11 20:49:29 -0500

    fix:  login

--- container/login/index.jsx
+++ container/login/index.jsx
@@ -1,64 +1,72 @@
-import { useFormTools } from 'components/BaseForm';
-import { RippleButton } from 'components/Ripple';
-import fetchJson from 'hooks/fetchJson';
-import Image from 'next/image';
-import { useRouter } from 'next/router';
-import React from 'react';
-import { Container, ContainerLeft, ContentImage, Form } from './styled';
+import { RippleButton } from "components/Ripple";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import {
+    useSetSession
+} from 'npm-pkg-hook';
+
+import { decodeToken } from "utils";
+import { Container, ContainerLeft, ContentImage, Form } from "./styled";
 
 export const Login = (props) => {
-    // STATES
-    const [handleChange, handleSubmitMain, setDataValue, { dataForm, errorForm, setForcedError }] = useFormTools()
-    const fileInputRef = React.useRef(null);
-    const formRef = React.useRef(null);
-    const router = useRouter()
-    const body = {
-        name: 'juvinaojesusd@gmail.com',
-        username: 'juvinaojesusd@gmail.com',
-        lastName: 'juvinaojesusd@gmail.com',
-        email: 'juvinaojesusd@gmail.com',
-        password: '113561675852804771364',
-        locationFormat: ' locationFormat[0]?.formatted_address',
-        useragent: 'window.navigator.userAgent',
-        deviceid: '23423423432',
-    }
+    const [handleSession] = useSetSession()
 
-    const handleForm = (e) => handleSubmitMain({
-        event: e,
-        action: () => {
-            return fetchJson(`${process.env.URL_BASE}/api/auth`, {
-                method: 'POST',
-                credentials: 'include',
-                headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify(body)
-            }).then(res => {
-                if (res.success) {
-                    window.localStorage.setItem('restaurant', res?.idStore)
-                    router.push('/dashboard')
-                }
-            }).catch(e => {
-            })
-        }
+  const router = useRouter();
+  const body = {
+    name: "juvinaojesusd@gmail.com",
+    username: "juvinaojesusd@gmail.com",
+    lastName: "juvinaojesusd@gmail.com",
+    email: "juvinaojesusd@gmail.com",
+    password: "113561675852804771364",
+    locationFormat: " locationFormat[0]?.formatted_address",
+    useragent: "window.navigator.userAgent",
+    deviceid: "23423423432",
+  };
+
+  const handleForm = async (e) => {
+    e.preventDefault();
+
+    const res = await fetch(`${process.env.URL_BACK_SERVER}/api/auth`, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(body),
+      credentials: "include",
     })
-    return (
-        <div>
-            <Container>
-                <ContainerLeft>
-                    <ContentImage>
-                        <Image
-                            objectFit='cover'
-                            layout='fill'
-                            src={'/images/sign-in_3f701ac0c6.png'}
-                            alt={"Picture of the author"}
-                            blurDataURL="/images/sign-in_3f701ac0c6.png"
-                            placeholder="blur"
-                        />
-                    </ContentImage>
-                </ContainerLeft>
-                <Form onSubmit={(e) => handleForm(e)}>
-                    <RippleButton widthButton='100%' margin='20px auto' type='submit'>Login</RippleButton>
-                </Form>
-            </Container>
-        </div>
-    )
-}
+    const { userId, token, success, idStore } = res || {};
+    if (success) {
+        const decode = decodeToken(token)
+        const cookiesDefault = [
+            { name: 'restaurant', value: idStore },
+            { name: 'usuario', value: decode?.id || id },
+            { name: 'session', value: token }
+          ]
+          await handleSession({ cookies: cookiesDefault })
+      window.localStorage.setItem("session", token);
+      window.localStorage.setItem("usuario", userId);
+      router.push("/dashboard");
+    }
+  };
+  return (
+    <div>
+      <Container>
+        <ContainerLeft>
+          <ContentImage>
+            <Image
+              objectFit="cover"
+              layout="fill"
+              src={"/images/sign-in_3f701ac0c6.png"}
+              alt={"Picture of the author"}
+              blurDataURL="/images/sign-in_3f701ac0c6.png"
+              placeholder="blur"
+            />
+          </ContentImage>
+        </ContainerLeft>
+        <Form onSubmit={(e) => handleForm(e)}>
+          <RippleButton widthButton="100%" margin="20px auto" type="submit">
+            Login
+          </RippleButton>
+        </Form>
+      </Container>
+    </div>
+  );
+};
